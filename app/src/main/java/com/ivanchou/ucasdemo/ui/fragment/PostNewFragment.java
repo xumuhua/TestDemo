763943e10eb9409943d44d18a9710fa91d7a9906
xumuhua <123138599@qq.com>
2015-03-08 16:13:29 +0800

    update DetailsFragment

--- app/src/main/java/com/ivanchou/ucasdemo/ui/fragment/PostNewFragment.java
+++ app/src/main/java/com/ivanchou/ucasdemo/ui/fragment/PostNewFragment.java
@@ -1,9 +1,40 @@
 package com.ivanchou.ucasdemo.ui.fragment;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.ivanchou.ucasdemo.ui.base.BaseFragment;
 
 /**
  * Created by ivanchou on 1/19/2015.
  */
 public class PostNewFragment extends BaseFragment {
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
